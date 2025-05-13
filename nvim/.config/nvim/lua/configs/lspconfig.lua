5f48f9735b75b257c129acdbe3eed4964cd8bd48
Piotr Płaczek <piotr.placzek@nexai.eu>
2025-05-13 09:54:29 +0200

    update lspconfig

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,24 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "ts_ls" }
 
--- read :h vim.lsp.config for changing options of lsp servers
+local custom_on_attach = function(client, bufnr)
+  nvlsp.on_attach(client, bufnr)
+
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
+
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+end
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = custom_on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
